f57037494af059c6368a888e5be68a730a3de93b
CatDBB101 <catdbb1000@gmail.com>
2023-12-03 20:36:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -19,10 +19,6 @@ app.use(
 const cors = require("cors");
 app.use(cors());
 
-// !: CookieParser
-const cookieParser = require("cookie-parser");
-app.use(cookieParser()); // initializing the lib
-
 // !: API path way section
 app.get("/api/", (req, res) => {
     res.send("Server is running...");
@@ -68,11 +64,6 @@ app.post("/api/login/", (req, res) => {
 
             if (data[0] && data[1]) {
                 console.log("[Action | GET] - Sent key to user");
-                res.setHeader(
-                    "Set-Cookie",
-                    "where=server;domain=localhost.com;Secure;"
-                );
-                res.cookie("MushroomLoginKey", key);
                 res.send(["ok", key]);
             } else if (data[0] && !data[1]) {
                 console.log("[Action | GET] - Password incorrect");
