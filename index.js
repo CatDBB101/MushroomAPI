0907f8e274de32cceb39000da76516200f9d3e22
CatDBB101 <catdbb1000@gmail.com>
2023-12-03 11:37:29 +0700

    ok

---
+++ index.js
@@ -0,0 +1,81 @@
+// !: Express
+const express = require("express");
+const app = express();
+
+// !: Axios & QueryString
+const axios = require("axios");
+const querystring = require("querystring");
+
+// !: Express request convert to json
+app.use(express.json());
+
+// !: Enable CORS
+const cors = require("cors");
+app.use(cors());
+
+// !: CookieParser
+const cookieParser = require("cookie-parser");
+app.use(cookieParser()); // initializing the lib
+
+// !: API path way section
+app.get("/api/", (req, res) => {
+    res.send("Server is running...");
+});
+
+app.get("/api/test", (req, res) => {
+    axios
+        .get("https://mushroom-db.vercel.app/")
+        .then((response) => {
+            let data = response.data;
+            if (data == "Server is running...") {
+                console.log("[Action | GET] - Database API is running");
+                res.send("Working");
+            } else {
+                console.log("[Action | GET] - Database API isn't running");
+                res.send("Not working");
+            }
+        })
+        .catch((error) => {
+            res.send("Error");
+            console.error("Error:", error);
+        });
+});
+
+app.get("/api/login/", (req, res) => {
+    var username = req.body.username;
+    var password = req.body.password;
+
+    var body = new URLSearchParams();
+    body.append("username", username);
+    body.append("password", password);
+
+    axios
+        .post("https://mushroom-db.vercel.app/users/key", body.toString(), {
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+        })
+        .then((response) => {
+            let data = response.data;
+            let key = data[2];
+            console.log(data);
+
+            if (data[0] && data[1]) {
+                console.log("[Action | GET] - Sent key to user");
+                res.cookie("MushroomLoginKey", key);
+                res.send("ok");
+            } else if (data[0] && !data[1]) {
+                console.log("[Action | GET] - Password incorrect");
+                res.send("Password incorrect");
+            } else {
+                console.log("[Action | GET] - Username not found");
+                res.send("Username not fonud");
+            }
+        })
+        .catch((error) => {
+            res.send("Error");
+            console.error("Error:", error);
+        });
+});
+
+app.listen(process.env.port || 3000); // Server lisening to localhost and port 3000
