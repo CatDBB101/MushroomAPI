6aaafa8d46e12bbb373e29794d54b1fef6e52787
CatDBB101 <catdbb1000@gmail.com>
2023-12-22 08:45:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const app = express();
 
-const cors = require("cors");
-app.use(cors());
+// const cors = require("cors");
+// app.use(cors());
+
+app.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "*");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    next();
+});
 
 app.use(express.json());
 
@@ -10,7 +17,6 @@ app.get("/api/", (req, res) => {
     res.json("Server is running.");
     console.log(req.body);
 });
-
 
 app.listen(process.env.port || 3000);
 module.exports = app;
