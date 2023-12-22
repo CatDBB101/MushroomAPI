80a48f43c25402691ef485e652ad09c57d75df49
CatDBB101 <catdbb1000@gmail.com>
2023-12-22 09:11:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const app = express();
 
-// const bodyParser = require("body-parser");
-// app.use(bodyParser.json());
-// app.use(
-//     bodyParser.urlencoded({
-//         extended: true,
-//     })
-// );
-
-app.use(express.json);
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
 
 const cors = require("cors");
 app.use(cors());
